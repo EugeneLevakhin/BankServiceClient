0f850eead3ad220a1114bf031d10d40a357ee34f
Eugene Levakhin <eugene.levakhin@gmail.com>
2019-12-15 00:45:41 +0200

    Added Add banknote and Get banknote functionality

--- BankServiceClient/Controllers/BanknotesController.cs
+++ BankServiceClient/Controllers/BanknotesController.cs
@@ -22,5 +22,64 @@ namespace BankServiceClient.Controllers
 
             return View(banknote);
         }
+
+        public ActionResult AddBanknote()
+        {
+            BanknoteModel model = new BanknoteModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult AddBanknote(BanknoteModel banknote)
+        {
+            if (banknote.Denomination < 1 || banknote.Quantity < 0)
+            {
+                return View("AddBanknoteError");
+            }
+
+            BanknotesServiceClient banknotesServiceClient = new BanknotesServiceClient();
+            bool success = banknotesServiceClient.AddBanknote(banknote.Denomination, banknote.Quantity);
+
+            if (!success)
+            {
+                return View("AddBanknoteError");
+            }
+
+            return Redirect("/Banknotes/GetAllBanknotes");
+        }
+
+        public ActionResult AddBanknoteError()
+        {
+            return View();
+        }
+
+        public ActionResult GetBanknoteInfo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetBanknoteInfo(string denomination)
+        {
+            if (int.TryParse(denomination.ToString(), out int value))
+            {
+                BanknotesServiceClient banknotesServiceClient = new BanknotesServiceClient();
+
+                BanknoteModel banknote = banknotesServiceClient.GetBanknote(value);
+
+                if (banknote != null)
+                {
+                    return RedirectToAction("GetBanknote", new { denomination = value });
+                }
+            }
+
+            return View("GetBanknoteError");
+        }
+
+        public ActionResult GetBanknoteError()
+        {
+            return View();
+        }
     }
 }
